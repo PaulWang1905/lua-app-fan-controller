6b4c3ae727907e839861b305f58d061b2cd7a984
Puyu Wang <puyuwang@live.com>
2019-05-29 17:34:47 +0000

    update copyright statement

--- luasrc/controller/fancontroller.lua
+++ luasrc/controller/fancontroller.lua
@@ -1,3 +1,6 @@
+-- Copyright 2019 Puyu Wang <puyuwang@live.com>
+-- Licensed to the public under the GNU General Public License v3.
+
 module("luci.controller.fancontroller", package.seeall)
 
 function index()
